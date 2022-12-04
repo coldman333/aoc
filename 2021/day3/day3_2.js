9b79c33e464ebb48dd8801320df37a7bd5653eb7
vteplyi <v.teplyi@betinvest.com>
2022-12-04 15:39:16 +0200

    old

--- 2021/day3/day3_2.js
+++ 2021/day3/day3_2.js
@@ -16,26 +16,9 @@ const day3task2 = () => {
 				if( parseInt( item[i]) !== i%2) {
 					return item;
 				}
-				// return : numsCopy[]
 			})
 			console.log('numsCopy',i, numsCopy);
 		}
-
 		console.log(numsCopy);
-
-	// .map(item => {
-	// 	item.map((num, i ) => {
-	// 		obj[`l${i}`] = Array.isArray(obj[`l${i}`]) ? obj[`l${i}`]:[];
-	// 		obj[`l${i}`].push(num);
-	// 	})
-	// })
-	// for (const [key, value] of Object.entries(obj)) {
-	// 	let sim =  obj[`${key}`][obj[`${key}`].sort().length/2]
-	// 	first += sim
-	// 	last += sim === '1'? '0':'1'
-	// }
-
-	//const result = '?'
-	// console.log("3 day 2 task \n result", result);
 };
 module.exports = day3task2;
--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-//  let day1task1 = require("./2021/day1/day1_1");
-//  let day1task2 = require("./2021/day1/day1_2");
-//  day1task1();
-//  day1task2();
-//
-// let day2task1 = require("./2021/day2/day2_1");
-// let day2task2 = require("./2021/day2/day2_2");
-// day2task1();
-// day2task2();
-
-
-  // let day3task1 = require("./2021/day3/day3_1");
- let task = require("./2022/day4/task_2");
-//   day3task1();
+let task = require("./2022/day4/task_2");
 task();
